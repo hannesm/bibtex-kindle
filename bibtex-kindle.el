76d66844f7cba14d7527044e87884dc6d8ad12ab
Hannes <hannes@mehnert.org>
2011-10-22 19:26:27 -0400

    some more code

--- bibtex-kindle.el
+++ bibtex-kindle.el
@@ -1,10 +1,39 @@
 ;;; bibtex-kinde.el --- Kindle utilities for BibTeX
 
+(require 'sha1)
+
 ;; prefix of files to push onto kindle
 (defvar bibtex-kindle-prefix "~/kindle")
 
 ;; path to pdftk binary
 (defvar bibtex-kindle-pdftk "/opt/local/bin/pdftk")
+
+
+(defun bibtex-kindle-export-collections ()
+  "Exports collections.json file from a bib file"
+  (interactive "P")
+  ; strategy: move at start of buffer
+  ; select all entries that have a kindle-file
+  ; use booktitle + year of InProceedings
+  ; use journal + issue of article
+  ; plus keywords
+  ; thus collections is a alist: collection -> [item]
+  (let (collections)
+    for x in bib*
+      if entry contains kindle-file key
+        if entry is inproceedings
+          collections[booktitle + year] ::= file
+        if entry is article
+          collections[journal] ::= file
+        if entry contains keywords
+          collections[keywords] ::= file
+    output {
+    for x in collections
+      output { + \" + car + @en-US\": { items:[
+      for y in cdr : output ,\"* sha1(/mnt/us/documents/bib/y) \"
+      output ],\"lastAccess\": now? + }}
+    output }
+))
 
 (defun get-pdf (ask-message default-location)
   ""
@@ -16,8 +45,6 @@
              (not (and (file-readable-p filename)
                        (string-equal (file-name-extension filename) "pdf")))))
     (list filename)))
-
-(require 'sha1)
 
 (defun bibtex-make-field-file (&optional arg)
   "Make a file field. That's enriched with metadata to put on the kindle."
@@ -36,6 +63,7 @@
           (let ((target-dir (format "%s/%s" bibtex-kindle-prefix year)))
             (make-directory target-dir t)
             (let ((target (format "%s/%s.pdf" target-dir (sha1 title))))
+              ; in a more perfect world, this would be firstauthor-title or similar
               (if (file-exists-p target)
                   (message (format "File with name %s already exists" target))
                 (progn
@@ -47,10 +75,8 @@
                     (process-send-string pdftkn (format "InfoValue: %s\n" title))
                     (process-send-eof pdftkn))))
                 (skip-chars-backward "}")
-                (insert (format "file:{%s}" target)))))))))
+                (insert (format "file:{%s/%s.pdf}" year (sha1 title))))))))))
 
 (global-set-key (kbd "C-c f") 'bibtex-make-field-file)
 
 (provide 'bibtex-kindle)
-(sha1 "foo")
-(concat (delq 10 (delq 9 (delq 32 (append "f  o\to" "ba  \nr" ())))))
